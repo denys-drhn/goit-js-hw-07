e19338bf9109d7ee02272e793c7a0bd196f26a9e
Denys <denys.drhn@gmail.com>
2023-02-09 19:53:34 +0200

    update task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,74 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+
+// 1. первий шаг -> создать разметку
+function createGalleryMarkup(items) {
+	return items.map(({preview, original, description}) => {
+		return `
+	<div class="gallery__item">
+	<a class="gallery__link" href="${original}">
+	<img
+		class="gallery__image"
+		src="${preview}"
+		alt="${description}"
+	/>
+	</a>
+	</div>
+	`;
+	}).join('');
+	
+};
+
+// 1. второй шаг ->  рендер разметки + создание переменних
+galleryContainer.innerHTML = galleryMarkup;
+
+
+
+function onGalleryContainerClick(event) {
+	event.preventDefault();	// *Запрети (перенаправлен на другую страницу) по умолчанию.
+	
+	// *нажатие только по тегу IMG:
+	if (event.target.nodeName !== "IMG") { 
+	return;
+	};
+}
+
+// 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery:
+// 4. добавь отображение подписей к изображениям из атрибута alt 
+// Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения:
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 }); 
+
+
+
+
+
+// When using the standalone variant (`simple-lightbox(.min).js`)
+
+// var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+// When using the standalone variant (`simple-lightbox(.min).js`)
+
+// var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+
+
+// Examples
+// let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // some usefull information
+// });
+
+// Example
+// var gallery = $('.gallery a').simpleLightbox();
+
+// gallery.next(); // Next Image
